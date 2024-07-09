10902736c51ee86188ff5567f112f96be84c72fa
Kaleidiana <kaleidiana63@gmail.com>
2024-07-09 12:23:55 +0300

    Added the files and did changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Components/Navbar'; // Ensure you have a Navbar component
+import Home from './Components/Home'; // Ensure you have a Home component
+import Login from './Components/Register'; // Import Login component
+import Signup from './Components/Login'; // Import Signup component
+import AddUser from './Components/Adduser'; // Import AddUser component
+import NotFound from './Components/NotFound'; // Ensure you have a NotFound component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <div className='content'>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/register">
+              <Login />
+            </Route>
+            <Route path="/login">
+              <Signup />
+            </Route>
+            <Route path="/adduser">
+              <AddUser />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h2>WELCOME !</h2>
+            <p>This is the users home page.</p>
+        
+        </div>
+    );
+};
+
+export default Home;
